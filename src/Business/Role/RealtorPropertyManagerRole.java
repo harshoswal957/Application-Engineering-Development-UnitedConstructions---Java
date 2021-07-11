fd4b32d9cf4fdbe377f75991272e47b484695ec6
harshoswal957 <oswal.h@northeastern.edu>
2021-07-10 20:11:15 -0400

    Realtor Property manager and Regulatory staff role

---
+++ src/Business/Role/RealtorPropertyManagerRole.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.role;
+
+import business.ecosystem.Ecosystem;
+import business.enterprise.Enterprise;
+import business.organization.Organization;
+import business.useraccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Harsh
+ */
+public class RealtorPropertyManagerRole extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Ecosystem business) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
